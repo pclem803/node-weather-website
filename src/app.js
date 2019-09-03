6c46ea361adf214c33a1718a6e473e7ed10febf2
Philip Clement <pclement803@gmail.com>
2019-09-03 18:52:12 -0500

    Updating footer

--- src/app.js
+++ src/app.js
@@ -24,14 +24,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Philip Clement'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Philip Clement'
     })
 })
 
@@ -39,7 +39,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Philip Clement'
     })
 })
 
@@ -85,7 +85,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Philip Clement',
         errorMessage: 'Help article not found.'
     })
 })
@@ -93,11 +93,11 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Philip Clement',
         errorMessage: 'Page not found.'
     })
 })
 
 app.listen(process.env.PORT, () => {
-    console.log('Server is up on port' + port + '.')
+    console.log('Server is up on port' + process.env.PORT + '.')
 })
